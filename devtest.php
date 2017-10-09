693e37b44c7f9df8e5fc638275dc0588f4bd299e
ArtiesFox <rush-hour_lee@hotmail.com>
2017-10-09 20:40:23 +0700

    Update devtest.php

--- devtest.php
+++ devtest.php
@@ -3,12 +3,16 @@ $greet = getenv('GREETING');
 echo $greet;
 
 include "lib/helper.php";
+echo "-- included";
+
 $dice = array("dice","ทอย","@d");
+echo "-- array \r\n";
 
 if(contains($test, $dice))
 {
   echo "true";
 }
+echo "-- if";
 
 echo $test;
 ?>
