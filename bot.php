077533047654d1a0ae2fb5838a08b9b01d739e69
ArtiesFox <rush-hour_lee@hotmail.com>
2017-10-09 16:03:54 +0700

    Update bot.php
    I'mma bad dogeee

--- bot.php
+++ bot.php
@@ -28,14 +28,12 @@ if (!is_null($events['events'])) {
 			{
 				$guide = Rand(1,9);
 			}
-			else
-			{
-				// Build message to reply back
-				$messages = [
-					'type' => 'text',
-					'text' => $guide
-				];
-			}
+			
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $guide
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
