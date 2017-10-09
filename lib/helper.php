f838d1d7b9a076a9fadfbb5936151e1bb7a28f82
ArtiesFox <rush-hour_lee@hotmail.com>
2017-10-09 22:09:45 +0700

    Update helper.php

--- lib/helper.php
+++ lib/helper.php
@@ -1,9 +1,33 @@
 <?php
-function contains($str, array $arr)
-{
-    foreach($arr as $a) {
-        if (stripos($str,$a) !== false) return true;
+    function contains($text, array $refword)
+    {
+        foreach($refword as $r)
+        {
+            if (stripos($text,$r) !== false) return true;
+        }
+        return false;
     }
-    return false;
-}
+
+    function startwith($text, $refword)
+    {
+        if (0 === strpos($text, $refword)) 
+        {
+            return true;
+        }
+        return false;
+    }
+
+    function startwithinarray($text, array $refword)
+    {
+        foreach($refword as $ref)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    function pickonefromarray(array $ref)
+    {
+        return $ref[mt_rand(0, count($ref) - 1)];
+    }
 ?>
