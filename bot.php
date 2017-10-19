4ac94efb7a26d2f2ad7a3de0834e1e3e68a0216f
ArtiesFox <rush-hour_lee@hotmail.com>
2017-10-19 20:59:42 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -6,7 +6,7 @@ $diceword = array("dice","ทอย","roll","โรล");
 $calword = array("cal","คิดเลข");
 $randfromcmd = array("pick","สุ่ม","โอน้อย");
 $debugword = array("debugmode");
-$haha = array("555", "ถถถ");
+$haha = array("555", "ถถถ", "๕๕๕", "haha", "lol");
 //----------------------------------------
 // Get POST body content
 $content = file_get_contents('php://input');
